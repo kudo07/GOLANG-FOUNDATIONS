43c9c38cba1619e4e2ac2f14eefeba89d4f67eeb
kudo07 <vknwl077@gmail.com>
2025-11-20 23:45:08 +0530

    which unix command

--- 1-INTRODUCTION/which.go
+++ 1-INTRODUCTION/which.go
@@ -9,10 +9,34 @@ import (
 func which() {
 	arguments := os.Args
 	if len(arguments) == 1 {
-		fmt.Println("please provide an argument")
+		fmt.Println("Please provide a command name as an argument")
 		return
 	}
 	file := arguments[1]
 	path := os.Getenv("PATH")
 	pathSplit := filepath.SplitList(path)
+	for _, directory := range pathSplit {
+		fullPath := filepath.Join(directory, file)
+
+		extensions := []string{""}
+		if filepath.Ext(file) == "" {
+			extensions = []string{"", ".exe", ".cmd", ".bat", ".com"}
+		}
+		for _, ext := range extensions {
+			testPath := fullPath + ext
+			fileInfo, err := os.Stat(testPath)
+			if err != nil {
+				continue
+			}
+			mode := fileInfo.Mode()
+			if !mode.IsRegular() {
+				continue
+			}
+			if mode&0111 != 0 || ext != "" {
+				fmt.Println(testPath)
+				return
+			}
+		}
+	}
+	fmt.Printf("%s not found in PATH\n", file)
 }
