c556a2085744f799d4055dc361d2398230aa20bc
kudo07 <vknwl077@gmail.com>
2025-11-22 23:53:30 +0530

    systemLog

--- 1-INTRODUCTION/systemLog.go
+++ 1-INTRODUCTION/systemLog.go
@@ -1,3 +1,4 @@
+// UNIX
 package main
 
 import (
@@ -5,10 +6,19 @@ import (
 	"log/syslog"
 )
 
-func ststemLog() {
+func systemLog() {
+	// Create a new connection to the system logger
 	sysLog, err := syslog.New(syslog.LOG_INFO|syslog.LOG_LOCAL7, "myGoApp")
 	if err != nil {
 		log.Println("Error creating syslog:", err)
 		return
 	}
+
+	// Send all log output to syslog
+	log.SetOutput(sysLog)
+
+	// Write a log message
+	log.Print("Everything is fine!")
 }
+
+// SYSLOG IS NOT SUPPORTTED IN WINDOWS
